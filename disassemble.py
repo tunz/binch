36caf73bfac94fab83685375e823e6b9739bb430
Choongwoo Han <cwhan.tunz@gmail.com>
2015-05-20 11:57:39 +0900

    first commit

---
+++ disassemble.py
@@ -0,0 +1,64 @@
+from capstone import *
+from elftools.elf.elffile import ELFFile
+from elftools.elf.sections import SymbolTableSection
+import sys
+
+class Disassembler():
+
+    memory = []
+
+    def __init__(self, filename):
+        self.loadELF(filename)
+
+    def readMemory(self, address, size):
+        for start, end, data in self.memory:
+            if address >= start and address <= end:
+                return data[address - start:size]
+        return ""
+
+    def loadELF(self, filename):
+        self.elf = ELFFile(file(sys.argv[1]))
+
+        # Load code segments
+        for elf_segment in self.elf.iter_segments():
+            if elf_segment.header.p_type != 'PT_LOAD':
+                continue
+
+            align = 0x1000
+            ELF_PAGEOFFSET = elf_segment.header.p_vaddr & (align-1)
+
+            memsz = elf_segment.header.p_memsz + ELF_PAGEOFFSET
+            offset = elf_segment.header.p_offset - ELF_PAGEOFFSET
+            filesz = elf_segment.header.p_filesz + ELF_PAGEOFFSET
+            vaddr = elf_segment.header.p_vaddr - ELF_PAGEOFFSET
+            memsz = (memsz + align ) & ~(align-1)
+
+            with open(sys.argv[1], 'rb') as f:
+                f.seek(offset, 0)
+                data = f.read(filesz)
+                self.memory.append((vaddr, vaddr+memsz, data))
+
+        self.entry = self.elf.header.e_entry
+
+        self.symtab = dict()
+        # Load symbol table
+        for section in self.elf.iter_sections():
+            if isinstance(section, SymbolTableSection):
+                for symbol in section.iter_symbols():
+                    if symbol['st_info']['type'] == 'STT_FUNC':
+                        self.symtab[symbol['st_value']] = symbol.name
+
+        arch = {'x86':CS_ARCH_X86,'x64': CS_ARCH_X86}[self.elf.get_machine_arch()]
+        mode = {'x86': CS_MODE_32, 'x64': CS_MODE_64}[self.elf.get_machine_arch()]
+        self.md = Cs(arch, mode)
+
+    def disassem(self, address):
+        count = 0
+        result=[]
+        for i in self.md.disasm(self.readMemory(address, 0x1000), 0x1000):
+            line = "0x%x\t%s\t%s" %(i.address - 0x1000 + address, i.mnemonic, i.op_str)
+            result.append(line)
+
+        return result
+
+
---
+++ view.py
@@ -0,0 +1,87 @@
+import urwid
+from disassemble import *
+
+class DisassembleText(urwid.Text):
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        if key == "j":
+            key = "down"
+        if key == "k":
+            key = "up"
+        return key
+
+class SymbolText(urwid.Text):
+
+    def selectable(self):
+        return False
+
+    def keypress(self, size, key):
+        return key
+
+class DisassembleList(urwid.SimpleListWalker):
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        return key
+
+class DisassembleView:
+    palette = [('header', 'white', 'black'),
+            ('reveal focus', 'black', 'light gray', 'standout'),]
+
+
+    footer_text = [
+            ('title', 'test'), '    ',
+            ('key', 'Q')
+            ]
+
+    def __init__(self, filename):
+        self.header = urwid.Text(filename)
+        palette = [('header', 'white', 'black'),
+                    ('reveal focus', 'black', 'dark cyan', 'standout'),]
+
+        da = Disassembler(filename)
+        body = da.disassem(da.entry)
+        items = []
+        for i in body:
+            address = int(i.split('\t')[0].lstrip('0x'),16)
+            if address in da.symtab:
+                items.append(SymbolText(" < "+da.symtab[address]+" >"))
+            items.append(urwid.Columns(
+                        [('fixed', 12, DisassembleText(i.split('\t')[0])),
+                            ('fixed', 10, DisassembleText(i.split('\t')[1])),
+                            DisassembleText(i.split('\t')[2])]))
+
+        items = map(lambda x: urwid.AttrMap(x, 'bg', 'reveal focus'), items)
+        walker = DisassembleList(items)
+
+        self.leftListbox = urwid.ListBox(walker)
+        self.leftListbox = urwid.Padding(self.leftListbox, ('fixed left',2), ('fixed right',1))
+        self.leftListbox = urwid.Filler(self.leftListbox, ('fixed top',1), ('fixed bottom',1))
+
+        self.rightListbox = urwid.ListBox([DisassembleText('test')])
+        self.rightListbox = urwid.Padding(self.rightListbox, ('fixed left',1), ('fixed right',2))
+        self.rightListbox = urwid.Filler(self.rightListbox, ('fixed top',1), ('fixed bottom',1))
+
+        self.body = urwid.Columns([self.leftListbox, ('fixed', 1, urwid.SolidFill("|")), self.rightListbox])
+
+        self.footer = urwid.AttrWrap(urwid.Text(self.footer_text), 'foot')
+        self.view = urwid.Frame(
+                urwid.AttrWrap(self.body, 'body'),
+                header=urwid.AttrWrap(self.header, 'head'),
+                footer=self.footer)
+
+    def main(self):
+        self.loop = urwid.MainLoop(self.view, self.palette,
+                handle_mouse=False,
+                unhandled_input=self.unhandled_input)
+        self.loop.run()
+
+    def unhandled_input(self, k):
+        if k in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
+        #if k in ('g', 'G'):
