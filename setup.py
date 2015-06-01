412cd4e227718c51a4a342625ad96c14ae39b6a8
Choongwoo Han <cwhan.tunz@gmail.com>
2015-06-01 12:38:20 +0900

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ py_modules = [
 
 setup (
         name = 'binch',
-        version = '0.2.1',
+        version = '0.2.2',
         description = 'a light ELF binary patch tool',
         author = 'Choongwoo Han',
         author_email = 'cwhan.tunz@gmail.com',
